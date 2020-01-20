bac0621b6d4cc9e24c670f3a51618685b758117c
Dmitry Barskov <barskoidim@gmail.com>
2020-01-21 00:22:22 +0300

    Merge `.dsl` methods to one file

--- app/src/main/java/com/example/todo/dsl/TodoDao.kt
+++ app/src/main/java/com/example/todo/dsl/TodoDao.kt
@@ -1,6 +1,7 @@
 package com.example.todo.dsl
 
 import com.example.todo.model.Todo
+import com.example.todo.model.TodoStatus
 import com.example.todo.persistance.TodoDao
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -19,3 +20,13 @@ fun TodoDao.save(todo: Todo): Single<Todo> {
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
 }
+
+fun TodoDao.findByStatus(status: TodoStatus): Single<List<Todo>> =
+    findByStatus(status.statusId)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+
+fun TodoDao.find(id: Long): Single<Todo> =
+    findById(id)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
